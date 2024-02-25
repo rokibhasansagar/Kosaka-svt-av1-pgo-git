0fa6342662fa97cc62f07ab6ae5974e702d792db
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2024-02-25 23:29:48 +0600

    Update encode.sh

--- encode.sh
+++ encode.sh
@@ -30,10 +30,52 @@ if test "$DOWNLOAD_OBJECTIVE_TYPE" == "none"; then
         echo -e "${red}[ERROR] No video files found inside video-input. Compilation requires at least '1' video inside the folder.${nc}"
         exit 1
     fi
+elif test "$DOWNLOAD_OBJECTIVE_TYPE" == "objective-3-fast"; then
+    mkdir -p "$PWD"/objective-3-fast
+    rm objective-*-fast/*640x360* objective-*-fast/*360p*
+    mv objective-*-fast/*.y4m objective-3-fast/ && rm -rf objective-[12]-fast
 fi
+
+export av1an_opts=(
+    --concat mkvmerge
+    --verbose
+    --split-method av-scenechange
+    --sc-method standard
+    --pix-format yuv420p10le
+    --sc-pix-format yuv420p
+    --sc-downscale-height 540
+)
 
 svt_encode() {
 shopt -s nullglob
+
+move_profraw() {
+    # shellcheck disable=SC2012
+    if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
+        for profraw in svt-pgo-data/*.profraw; do
+            basename_profraw="${profraw##*/}"
+            mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
+        done
+    fi
+}
+
+move_fdata() {
+    #This might not be needed with --instrumentation-file-append-pid
+    if test "$BOLT" == "true"; then
+        echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
+        # shellcheck disable=SC2012
+        if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
+            echo -e "${green}Found fdata file in svt-bolt-data${nc}"
+            for fdata in svt-bolt-data/*.fdata; do
+                basename_fdata="${fdata##*/}"
+                mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
+            done
+        else
+            echo -e "${red}No fdata file found in svt-bolt-data${nc}"
+        fi
+    fi
+}
+
 for file in "$PWD"/{video-input,objective-*}/*.{mkv,mp4,y4m}; do
     echo ""
     basename="${file##*/}"
@@ -41,205 +83,46 @@ for file in "$PWD"/{video-input,objective-*}/*.{mkv,mp4,y4m}; do
     export PATH="$PWD/$_repo/Bin/Release:$PATH"
     if test "$SVT_AV1AN_COMMAND"; then
         echo -e "${green}Encoding:${nc}${white} $basename${nc} with ${white}SVT_AV1AN_COMMAND${nc}"
-        av1an \
-        -e svt-av1 \
-        --concat mkvmerge \
-        --verbose \
-        --split-method av-scenechange \
-        --sc-method standard \
-        --pix-format yuv420p10le \
-        --sc-pix-format yuv420p \
-        --sc-downscale-height 540 \
-        -v " $SVT_AV1AN_COMMAND " \
-        -i "$file" \
-        -o "$file.1.av1an"
+        av1an -e svt-av1 ${av1an_opts[@]} -v " $SVT_AV1AN_COMMAND " -i "$file" -o "$file.1.av1an"
 
-        # shellcheck disable=SC2012
-        if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
-            for profraw in svt-pgo-data/*.profraw; do
-                basename_profraw="${profraw##*/}"
-                mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
-            done
-        fi
-
-        #This might not be needed with --instrumentation-file-append-pid
-        if test "$BOLT" == "true"; then
-            echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
-            # shellcheck disable=SC2012
-            if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
-                echo -e "${green}Found fdata file in svt-bolt-data${nc}"
-                for fdata in svt-bolt-data/*.fdata; do
-                    basename_fdata="${fdata##*/}"
-                    mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
-                done
-            else
-                echo -e "${red}No fdata file found in svt-bolt-data${nc}"
-            fi
-        fi
+        move_profraw
+        move_fdata
     fi
 
     #command 2
     if test "$SVT_AV1AN_COMMAND_2"; then
         echo -e "${green}Encoding:${nc}${white} $basename${nc} with ${white}SVT_AV1AN_COMMAND_2${nc}"
-        av1an \
-        -e svt-av1 \
-        --concat mkvmerge \
-        --verbose \
-        --split-method av-scenechange \
-        --sc-method standard \
-        --pix-format yuv420p10le \
-        --sc-pix-format yuv420p \
-        --sc-downscale-height 540 \
-        -v " $SVT_AV1AN_COMMAND_2 " \
-        -i "$file" \
-        -o "$file.2.av1an"
+        av1an -e svt-av1 ${av1an_opts[@]} -v " $SVT_AV1AN_COMMAND_2 " -i "$file" -o "$file.2.av1an"
         
-        # shellcheck disable=SC2012
-        if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
-            for profraw in svt-pgo-data/*.profraw; do
-                basename_profraw="${profraw##*/}"
-                mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
-            done
-        fi
-
-        #This might not be needed with --instrumentation-file-append-pid
-        if test "$BOLT" == "true"; then
-            echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
-            # shellcheck disable=SC2012
-            if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
-                echo -e "${green}Found fdata file in svt-bolt-data${nc}"
-                for fdata in svt-bolt-data/*.fdata; do
-                    basename_fdata="${fdata##*/}"
-                    mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
-                done
-            else
-                echo -e "${red}No fdata file found in svt-bolt-data${nc}"
-            fi
-        fi
-
+        move_profraw
+        move_fdata
     fi
 
     #command 3
     if test "$SVT_AV1AN_COMMAND_3"; then
         echo -e "${green}Encoding:${nc}${white} $basename${nc} with ${white}SVT_AV1AN_COMMAND_3${nc}"
-        av1an \
-        -e svt-av1 \
-        --concat mkvmerge \
-        --verbose \
-        --split-method av-scenechange \
-        --sc-method standard \
-        --pix-format yuv420p10le \
-        --sc-pix-format yuv420p \
-        --sc-downscale-height 540 \
-        -v " $SVT_AV1AN_COMMAND_3 " \
-        -i "$file" \
-        -o "$file.3.av1an"
+        av1an -e svt-av1 ${av1an_opts[@]} -v " $SVT_AV1AN_COMMAND_3 " -i "$file" -o "$file.3.av1an"
 
-        # shellcheck disable=SC2012
-        if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
-            for profraw in svt-pgo-data/*.profraw; do
-                basename_profraw="${profraw##*/}"
-                mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
-            done
-        fi
-
-        #This might not be needed with --instrumentation-file-append-pid
-        if test "$BOLT" == "true"; then
-            echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
-            # shellcheck disable=SC2012
-            if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
-                echo -e "${green}Found fdata file in svt-bolt-data${nc}"
-                for fdata in svt-bolt-data/*.fdata; do
-                    basename_fdata="${fdata##*/}"
-                    mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
-                done
-            else
-                echo -e "${red}No fdata file found in svt-bolt-data${nc}"
-            fi
-        fi
-
+        move_profraw
+        move_fdata
     fi
 
     #command 4
     if test "$SVT_AV1AN_COMMAND_4"; then
         echo -e "${green}Encoding:${nc}${white} $basename${nc} with ${white}SVT_AV1AN_COMMAND_4${nc}"
-        av1an \
-        -e svt-av1 \
-        --concat mkvmerge \
-        --verbose \
-        --split-method av-scenechange \
-        --sc-method standard \
-        --pix-format yuv420p10le \
-        --sc-pix-format yuv420p \
-        --sc-downscale-height 540 \
-        -v " $SVT_AV1AN_COMMAND_4 " \
-        -i "$file" \
-        -o "$file.4.av1an"
+        av1an -e svt-av1 ${av1an_opts[@]} -v " $SVT_AV1AN_COMMAND_4 " -i "$file" -o "$file.4.av1an"
 
-        # shellcheck disable=SC2012
-        if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
-            for profraw in svt-pgo-data/*.profraw; do
-                basename_profraw="${profraw##*/}"
-                mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
-            done
-        fi
-
-        #This might not be needed with --instrumentation-file-append-pid
-        if test "$BOLT" == "true"; then
-            echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
-            # shellcheck disable=SC2012
-            if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
-                echo -e "${green}Found fdata file in svt-bolt-data${nc}"
-                for fdata in svt-bolt-data/*.fdata; do
-                    basename_fdata="${fdata##*/}"
-                    mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
-                done
-            else
-                echo -e "${red}No fdata file found in svt-bolt-data${nc}"
-            fi
-        fi
-
+        move_profraw
+        move_fdata
     fi
 
     #command 5
     if test "$SVT_AV1AN_COMMAND_5"; then
         echo -e "${green}Encoding:${nc}${white} $basename${nc} with ${white}SVT_AV1AN_COMMAND_5${nc}"
-        av1an \
-        -e svt-av1 \
-        --concat mkvmerge \
-        --verbose \
-        --split-method av-scenechange \
-        --sc-method standard \
-        --pix-format yuv420p10le \
-        --sc-pix-format yuv420p \
-        --sc-downscale-height 540 \
-        -v " $SVT_AV1AN_COMMAND_5 " \
-        -i "$file" \
-        -o "$file.5.av1an"
+        av1an -e svt-av1 ${av1an_opts[@]} -v " $SVT_AV1AN_COMMAND_5 " -i "$file" -o "$file.5.av1an"
 
-        # shellcheck disable=SC2012
-        if ! test "$(ls svt-pgo-data/*.profraw 2>/dev/null | wc -l)" -eq 0; then
-            for profraw in svt-pgo-data/*.profraw; do
-                basename_profraw="${profraw##*/}"
-                mv "$profraw" svt-pgo-data/"$basename_profraw"."$(echo "$RANDOM" | md5sum | head -c 5)".profraw-real
-            done
-        fi
-
-        #This might not be needed with --instrumentation-file-append-pid
-        if test "$BOLT" == "true"; then
-            echo -e "${green}Bolt is enabled checking for .fdata file(s)${nc}"
-            # shellcheck disable=SC2012
-            if ! test "$(ls svt-bolt-data/*.fdata 2>/dev/null | wc -l)" -eq 0; then
-                echo -e "${green}Found fdata file in svt-bolt-data${nc}"
-                for fdata in svt-bolt-data/*.fdata; do
-                    basename_fdata="${fdata##*/}"
-                    mv "$fdata" svt-bolt-data/"$basename_fdata"."$(echo "$RANDOM" | md5sum | head -c 5)".fdata-real
-                done
-            else
-                echo -e "${red}No fdata file found in svt-bolt-data${nc}"
-            fi
-        fi
-
+        move_profraw
+        move_fdata
     fi
 done
 
