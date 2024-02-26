92a31c0a7f0b6b858740e6682dd87b2e9c7f013d
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2024-02-26 17:53:40 +0600

    Update encode.sh

--- encode.sh
+++ encode.sh
@@ -33,8 +33,8 @@ if test "$DOWNLOAD_OBJECTIVE_TYPE" == "none"; then
 elif test "$DOWNLOAD_OBJECTIVE_TYPE" == "objective-3-fast"; then
     mkdir -p "$PWD"/objective-3-fast
     rm -f objective-*/*640x360* objective-*/*360p* objective-*/*240p*
-    mv -f objective-1/*.y4m objective-3-fast/ 2>/dev/null
-    mv -f objective-2/*.y4m objective-3-fast/ 2>/dev/null
+    mv -f objective-1-fast/*.y4m objective-3-fast/ 2>/dev/null
+    mv -f objective-2-fast/*.y4m objective-3-fast/ 2>/dev/null
     rm -rf -- "$PWD"/objective-1-fast "$PWD"/objective-2-fast
 fi
 
